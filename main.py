ee4b44c57935d14f73e569f3b69d7359831a25a0
bawejahritik <bawejahritik@gmail.com>
2024-02-09 20:51:08 -0500

    Complete get and create account

--- main.py
+++ main.py
@@ -1,17 +1,65 @@
-passwords = {}
+import click
+import os
+import json
+
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+
+def create_config_if_not_exists():
+    if not os.path.isfile(".env"):
+        key = Fernet.generate_key()
+        key = key.decode("utf-8")
+        with open(".env", "w") as f:
+            f.write(f"key={key}")
+
+def encode_password(password):
+    key = os.getenv("key")
+    cipher_suite = Fernet(key)
+    encoded_text = cipher_suite.encrypt(password.encode("utf-8"))
+    
+    return encoded_text.decode()
+
+def decode_password(encoded_password):
+    key = os.getenv("key")
+    cipher_suite = Fernet(key)
+    decoded_text = cipher_suite.decrypt(encoded_password)
+    return decoded_text.decode("utf-8")
+
+def checkFileExists():
+    if not os.path.isfile("passwords.json"):
+        with open('passwords.json', 'w') as fp:
+            pass
+
+def read_from_json():
+    with open("passwords.json", "r") as f:
+        data = json.load(f)
+    return data
+
+def write_to_json(dict_to_add):
+    json_object = json.dumps(dict_to_add, indent=4)
+    with open('passwords.json', 'w+') as fp:
+        fp.write(json_object)
 
 def create_new_password(account, user_name, password):
+    checkFileExists()
+    passwords = read_from_json()
+
     if account in passwords:
-        raise Exception("Account already exists. Please get the password or update the exisiting password \n")
+        return Exception("Account already exists. Please get the password or update the exisiting password \n")
     
+    encoded_password = encode_password(password)
+
     new_dict = {
         "user_name": user_name,
-        "password": password
+        "password": encoded_password
     }
 
     passwords[account] = new_dict
 
+    write_to_json(passwords)
+
 def get_existing_password(account):
+    passwords = read_from_json()
     if account not in passwords:
         raise Exception("Account doesn't exist, create a new password for this account \n")
     
@@ -19,33 +67,37 @@ def get_existing_password(account):
     username = curr_account["user_name"]
     password = curr_account["password"]
 
+    password = decode_password(password)
+
     return username, password
 
+
+@click.command()
+@click.option('-c', '--create',nargs=3,type=str, help='create a new entry with the following args account username password')
+@click.option('-g', '--get',nargs=1, help='get an existing entry')
+def main(create, get):
+
+    create_config_if_not_exists()
+    load_dotenv()
+
+    if create:
+        try:
+            create_new_password(*create)
+            print("Successfully Added!")
+        except Exception as e:
+            print(e)
+        except:
+            print("Error adding the account, please try again later.")
+    
+    if get:
+        try:
+            user_name, password = get_existing_password(get)
+            print(f"{user_name} {password}")
+        except Exception as e:
+            print(e)
+        except:
+            print("Error getting the password, please try again.")
+
+
 if __name__ == "__main__":
-    option = int(input(f"Press 1 to add a password \nPress 2 to get an existing password \nPress 3 to exit the program \n"))
-
-    while option != 3:
-        if option == 1:
-            account = input("Enter the account: ")
-            username = input("Enter the username: ")
-            password = input("Enter the password: ")
-
-            try:
-                create_new_password(account, username, password)
-                print("Successful")
-            except Exception as e:
-                print(e)
-            except:
-                print("Failed. Please try later!")
-
-        elif option == 2:
-            account = input("Enter the account: ")
-            try:
-                username, password = get_existing_password(account)
-                print(f"Username: {username} \nPassword: {password} \n\n")
-            except Exception as e:
-                print(e)
-            except:
-                print(f"Failed to retrieve the username and password for {account}")
-        
-        option = int(input(f"Press 1 to add a password \nPress 2 to get an existing password \nPress 3 to exit the program \n"))
+    main()
